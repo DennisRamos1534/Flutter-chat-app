e31a61e410243f6f8ca14aeec32c7e784504c8be
Dennis Ramos <dsrg1995@gmail.com>
2021-10-17 10:23:07 -0500

    Fin seccion 7

---
+++ lib/models/usuario.dart
@@ -0,0 +1,17 @@
+
+
+class Usuario {
+
+  bool  online;
+  String? email;
+  String? nombre;
+  String? uid;
+
+  Usuario({
+    required this.online, 
+    this.email, 
+    this.nombre, 
+    this.uid,     
+  });
+
+}
